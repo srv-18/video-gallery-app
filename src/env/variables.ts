aa7df0f0b8786676e93dd6efee3ec1bf1b491cb8
srv-18 <shivamverma862@gmail.com>
2025-06-02 15:49:48 +0000

    setup cors

--- src/env/variables.ts
+++ src/env/variables.ts
@@ -1,14 +1,15 @@
 import "dotenv/config";
 
 const env = {
-    PORT: process.env.PORT || "",
-    DB_URL: process.env.DB_URL || "",
-    JWT_SECRET: process.env.JWT_SECRET || "",
+    PORT: process.env.PORT,
+    DB_URL: process.env.DB_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
     S3_ENDPOINT: process.env.S3_ENDPOINT,
     S3_REGION: process.env.S3_REGION,
     S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
     S3_SECRET_KEY: process.env.S3_SECRET_KEY,
-    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    FRONTEND_URL: process.env.FRONTEND_URL
 };
 
 for (let key in env) {
--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: [`${env.FRONTEND_URL}` ,  'http://127.0.0.1:5173', 'http://localhost:5173'],
     credentials: true
 }));
 app.use(cookieParser());
